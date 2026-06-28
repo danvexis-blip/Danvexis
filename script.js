ca621fc4aa460af42d85291e059703174b6d641b
Danvexis <danvexis@gmail.com>
2026-06-28 22:29:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-container.innerHTML += `
-  <div class="product">
-    <img src="${p.image}" />
-    <h3>${p.name}</h3>
-    <p>$${p.price}</p>
-    <p>Stock: ${p.stock}</p>
-    <button onclick="buy('${id}', ${p.stock})">Buy</button>
-  </div>
-`;
